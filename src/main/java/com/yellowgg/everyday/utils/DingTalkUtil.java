7346180cf7f1ca567863a3c27094d884186bed9c
yellowgg <yellowgggg@gmail.com>
2020-06-17 12:45:35 +0800

    钉钉工具类

--- src/main/java/com/yellowgg/everyday/utils/DingTalkUtil.java
+++ src/main/java/com/yellowgg/everyday/utils/DingTalkUtil.java
@@ -25,30 +25,30 @@ public class DingTalkUtil {
     /**
      * 加签方式：获取机器人的webhookUrl
      *
-     * @param hookUrl 初始化的url，既钉钉那边复制的
-     * @param secret  密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
+     * @param webHookUrl 初始化的url，既钉钉那边复制的
+     * @param key        密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
      * @return 拼接好的完整的webhookUrl
      */
-    private static String getHookUrl(String hookUrl, String secret) throws Exception {
+    private static String getHookUrl(String webHookUrl, String key) throws Exception {
         Long timestamp = System.currentTimeMillis();
         String sign = null;
-        if (StrUtil.isNotBlank(secret)) {
-            sign = getSign(timestamp, secret);
+        if (StrUtil.isNotBlank(key)) {
+            sign = getSign(timestamp, key);
         }
-        return String.format("%s&timestamp=%s&sign=%s", hookUrl, timestamp, sign);
+        return String.format("%s&timestamp=%s&sign=%s", webHookUrl, timestamp, sign);
     }
 
     /**
      * 获取签名
      *
      * @param timestamp 当前时间戳
-     * @param secret    密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
+     * @param key       密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
      * @return 签名值
      */
-    private static String getSign(Long timestamp, String secret) throws Exception {
-        String stringToSign = timestamp + "\n" + secret;
+    private static String getSign(Long timestamp, String key) throws Exception {
+        String stringToSign = timestamp + "\n" + key;
         Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(new SecretKeySpec(secret.getBytes(CODE), "HmacSHA256"));
+        mac.init(new SecretKeySpec(key.getBytes(CODE), "HmacSHA256"));
         byte[] signData = mac.doFinal(stringToSign.getBytes(CODE));
         return URLEncoder.encode(new String(Base64Utils.encode(signData)), CODE);
     }
@@ -56,13 +56,13 @@ public class DingTalkUtil {
     /**
      * 发送text类型消息
      *
-     * @param hookUrl 初始化的url，既钉钉那边复制的
-     * @param content 要发送的内容
-     * @param secret  密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
+     * @param webHookUrl 初始化的url，既钉钉那边复制的
+     * @param content    要发送的内容
+     * @param key        密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
      */
-    public static void sendTextMsg(String hookUrl, String content, String secret) throws Exception {
-        hookUrl = getHookUrl(hookUrl, secret);
-        DingTalkClient client = new DefaultDingTalkClient(hookUrl);
+    public static void sendTextMsg(String webHookUrl, String key, String content) throws Exception {
+        webHookUrl = getHookUrl(webHookUrl, key);
+        DingTalkClient client = new DefaultDingTalkClient(webHookUrl);
         OapiRobotSendRequest request = new OapiRobotSendRequest();
         request.setMsgtype("text");
         OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
