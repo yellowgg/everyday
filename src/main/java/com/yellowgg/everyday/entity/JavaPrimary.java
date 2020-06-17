b76944fe94dd43c6a4e9da768343ad0f458890e8
yellowgg <yellowgggg@gmail.com>
2020-06-17 13:06:17 +0800

    实体类

---
+++ src/main/java/com/yellowgg/everyday/entity/JavaPrimary.java
@@ -0,0 +1,14 @@
+package com.yellowgg.everyday.entity;
+
+import com.yellowgg.everyday.entity.base.BaseEntity;
+import lombok.Data;
+
+/**
+ * @Description: java初级面试题
+ * @Author: yellowgg
+ * @Date: Created in 2020/6/17 12:59
+ */
+@Data
+public class JavaPrimary extends BaseEntity {
+    private String content;
+}
