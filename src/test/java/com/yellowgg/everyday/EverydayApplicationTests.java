eae2d8d5dfca2091563ea7958a793b9827faa87b
yellowgg <yellowgggg@gmail.com>
2020-06-17 10:34:45 +0800

    项目初始化

---
+++ src/test/java/com/yellowgg/everyday/EverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellowgg.everyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
